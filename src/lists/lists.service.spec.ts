81db27181f5b5a9091e077378569ee7ea3ef84e3
Luan10017 <luan10017@hotmail.com>
2025-02-04 21:59:55 -0300

    add tests

--- src/lists/lists.service.spec.ts
+++ src/lists/lists.service.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ListsService } from './lists.service';
 
+const mockeList = {
+  create: jest.fn(),
+}
+
+const mockHttpService = {
+  post: jest.fn(),
+}
+
 describe('ListsService', () => {
   let service: ListsService;
-
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ListsService],
-    }).compile();
-
-    service = module.get<ListsService>(ListsService);
+    service = new ListsService(mockeList as any, mockHttpService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  it('deve criar uma lista', ()=> {
+
+  })
+  // let service: ListsService;
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     providers: [ListsService],
+  //   }).compile();
+  //   service = module.get<ListsService>(ListsService);
+  // });
+  // it('should be defined', () => {
+  //   expect(service).toBeDefined();
+  // });
+
